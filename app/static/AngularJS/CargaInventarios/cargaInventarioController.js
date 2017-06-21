dcc611ec0dcd5a530501affb788b9536624e3c1d
Sahirely Yam <syam@bism.com.mx>
2017-06-20 19:03:26 -0500

    carga inventario

--- app/static/AngularJS/CargaInventarios/cargaInventarioController.js
+++ app/static/AngularJS/CargaInventarios/cargaInventarioController.js
@@ -1,9 +1,8 @@
 registrationModule.controller('cargaInventarioController', function($scope, $rootScope, $location, userFactory, alertFactory) {
 
-
     $scope.init = function() {
       userFactory.ValidaSesion();
+      $scope.userData = userFactory.getUserData();
     }
-
 
 });
