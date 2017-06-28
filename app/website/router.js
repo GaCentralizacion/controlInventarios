e21b1b63053b269e59a20e764071cc47a5e69a80
agrijalva <agrijalva@bism.com.mx>
2017-06-28 12:29:17 -0500

    Envio de Notificación

--- app/website/router.js
+++ app/website/router.js
@@ -1,5 +1,6 @@
  module.exports = {
     login: require('./controllers/login'),
     layout: require('./controllers/layout'),
-    cargaInventario: require('./controllers/cargaInventario')
+    cargaInventario: require('./controllers/cargaInventario'),
+    notificacion: require('./controllers/notificacion')
  }
